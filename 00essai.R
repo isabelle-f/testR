b3be5c092ed3b7e529cbf728e1fcf498f2aba0da
Isabelle Falleau <isabelle.falleau@mnhn.fr>
2021-07-21 15:10:26 +0200

    fichier créé avec RStudio et mis dans github

---
+++ 00essai.R
@@ -0,0 +1,24 @@
+x <- 2 # crée l’objet x en lui donnant la valeur 2
+X = 4 # X reçoit la valeur 4
+b = x # b reçoit la valeur x, soit ici 2
+3 -> y # y reçoit la valeur 3
+objects()
+ls()
+rm(b,l)
+ls()
+data("iris")
+View(iris)
+rien = NULL
+temperature = 23.5
+pluie = TRUE
+tempete = F
+station = 'Paris'
+ville = "Corbeil-Essonnes"
+mode(rien)
+mode(temperature)
+is.null(rien)
+is.logical(rien)
+is.numeric(rien)
+is.complex(rien)
+is.character(rien)
+attributes(pluie)
